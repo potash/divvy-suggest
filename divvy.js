67972d2c83386dea81f252ccde5235f9de08c1ae
Eric Potash <eric@k2co3.net>
2014-01-04 13:59:14 -0600

    reorganize

---
+++ divvy.js
@@ -0,0 +1,79 @@
+var map, layer, heatmap, heatmapData, markers;
+var fromProjection, toProjection;
+
+var initPages = 22;
+var initUrl = "http://suggest.divvybikes.com/api/places";
+
+function fetchPlaces(url, fetchNext) {
+	$.ajax({dataType: "json", url: url,
+		success: function(data) {
+			if (fetchNext && data.metadata.next != null) {	
+				fetchPlaces(data.metadata.next, fetchNext);
+			}
+			addPlaces(data); 
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			console.log(errorThrown);
+		}
+	});
+}
+
+function init(divId) {
+	initHeatmap(divId);
+	addStations();
+	/*fetchPlaces(initUrl);
+	for(var i = 2; i < initPages; i++) {
+		fetchPlaces(initUrl + "?page=" + i);
+	}
+	fetchPlaces(initUrl + "?page=" + initPages, true);*/
+}
+
+function addPlaces(places) {
+	heatmapData.data = heatmapData.data.concat(
+			places.features.map(function(p) {
+				var support = p.properties.submission_sets.support == null ? 1 : p.properties.submission_sets.support.length;
+				heatmapData.max = Math.max(heatmapData.max, support);
+				return {
+					lonlat: new OpenLayers.LonLat(p.geometry.coordinates[0], p.geometry.coordinates[1]), 
+				count: support
+				};
+			}) 
+			);
+	heatmap.setDataSet(heatmapData);
+}
+
+function addStations() {
+	var size = new OpenLayers.Size(21,25);
+	var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
+	var icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png', size, offset);
+
+	getStations().stationBeanList.forEach(function(s) {
+		markers.addMarker(new OpenLayers.Marker(
+				new OpenLayers.LonLat(s.longitude,s.latitude).transform(fromProjection,toProjection),
+				icon.clone())
+			);
+	});
+}
+
+function initHeatmap(divId) {
+	heatmapData = { max: 1, data : [] };
+	map = new OpenLayers.Map(divId);
+	layer = new OpenLayers.Layer.OSM();
+
+	fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
+	toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
+	var chicagoLonLat = new OpenLayers.LonLat(-87.629767, 41.878247);
+
+	// create our heatmap layer
+	heatmap = new OpenLayers.Layer.Heatmap( "Heatmap Layer", map, layer, {visible: true, radius:30}, 
+			{isBaseLayer: false, opacity: 0.3, projection: fromProjection});
+	map.addLayers([layer, heatmap]);
+
+	map.setCenter(chicagoLonLat.transform(fromProjection,toProjection), 9);
+	heatmap.setDataSet(heatmapData);
+
+	markers = new OpenLayers.Layer.Markers( "Markers" );
+	map.addLayer(markers);
+
+}
+
